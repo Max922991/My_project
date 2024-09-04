1323ef45fc9f8a1066f3acb2ffde60b1cb765333
Max922991 <max43223@gmail.com>
2024-09-04 16:12:02 +0300

    Initial commit

---
+++ core/src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testModule1() {
+        System.out.println("Тест из модуля module1!");
+    }
+}
---
+++ db/src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testModule3() {
+        System.out.println("Тест из модуля module3!");
+    }
+}
